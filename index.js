2e51a0276e9d78a833d7961890a6c6700deed3b1
Adriano Araújo Costa <adriano.costa@fiap.com.br>
2022-02-09 22:43:29 -0300

    adicionada função de frases

--- index.js
+++ index.js
@@ -2,14 +2,40 @@ document.querySelector('#botao').addEventListener('click', function () {
 
     let peso    = document.querySelector('#peso'  ).value;
     let altura  = document.querySelector('#altura').value;
-    let frase   = document.createElement('span');
+    let frase   = document.createElement('span'   );
 
     let imc     = peso / (altura * altura);
 
-    frase.textContent = 'IMC: ' +  Math.round(imc) ;
+    frase.textContent = 'Seu IMC é de  ' +  Math.round(imc) + ', logo ' + criarFrase(imc);
 
     let resultado = document.querySelector('.resultado');
 
     resultado.appendChild(frase);
 
 });
+
+function criarFrase(value)
+{
+    let text = "informe novamente os valores";
+
+    if (value < 20)   {  
+        text ='Abaixo do peso ideal';
+    }  
+    else if (value > 20 && value <= 25)   {  
+        text ="Peso Ideal";
+    }  
+    else if (value > 25 && value <= 30)   {  
+        text ="Acima do peso ideal";
+    }  
+    else if (value > 30 && value <= 35)   {  
+        text ="Inicio da obesidade";
+    }  
+    else if (value > 35 && value <= 40)   {  
+        text ="Obesidade preocupante";
+    }  
+    else if (value > 40 && value <= 50)   {  
+        text ="Obesidade Morbida";
+    }
+
+    return text;
+}
